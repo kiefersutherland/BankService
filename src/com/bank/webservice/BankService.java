3bef865e6ad3d1eaa8f8473135b9ff371fe9bc77
kiefersutherland <Administrator@DELL-PC>
2017-06-05 10:45:06 +0800

    new

--- src/com/bank/webservice/BankService.java
+++ src/com/bank/webservice/BankService.java
@@ -7,6 +7,9 @@ import java.io.UnsupportedEncodingException;
 import java.security.KeyStore;
 import java.security.PublicKey;
 import java.security.Signature;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import sun.misc.BASE64Decoder;
 
@@ -21,7 +24,7 @@ public class BankService {
 	    	
     	  String keyStoreType = "JKS";  
      
-     
+     //test for new
             
           KeyStore keystore = KeyStore.getInstance(keyStoreType);  
           keystore.load(new FileInputStream(new File(keystoreFile)), password.toCharArray());  
@@ -117,5 +120,67 @@ public class BankService {
 
 		return outbuf;
 	}
+	
+	
+	
+	  public static final String CHARSET = "GBK";
+	  private static final String fmtTime = "yyyyMMddHHmmss";
+    /**
+     * 组装报文
+     * 这里append比较多，是为了展现报文头的各个字段；实际使用中请按需减少
+     * 
+     * @param yqdm 20位银企代码
+     * @param bsnCode 交易代码
+     * @param xmlBody xml主体报文
+     * @return
+     */
+    public static String asemblyPackets(String yqdm, String bsnCode, String xmlBody) {
+        
+        Date now = Calendar.getInstance().getTime();
+        
+        StringBuilder buf = new StringBuilder();
+        buf.append("A00101");
+        
+        //编码
+        String encoding = "01";
+        if (CHARSET.equalsIgnoreCase("GBK")) {
+            encoding = "01";
+        } else if(CHARSET.equalsIgnoreCase("utf-8") || CHARSET.equalsIgnoreCase("utf8")) {
+            encoding = "02";
+        }
+        buf.append(encoding);//编码
+        
+        buf.append("01");//通讯协议为TCP/IP
+        buf.append(String.format("%20s", yqdm));//银企代码
+        try {
+            buf.append(String.format("%010d", xmlBody.getBytes(CHARSET).length));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        buf.append(String.format("%-6s", bsnCode));//交易码-左对齐
+        buf.append("12345");//操作员代码-用户可自定义
+        buf.append("01");//服务类型 01请求
+        
+        String fmtNow = new SimpleDateFormat(fmtTime).format(now);
+        buf.append(fmtNow); //请求日期时间
+        
+        String requestLogNo = "YQTEST" + fmtNow;
+        buf.append(requestLogNo);//请求方系统流水号
+        
+        buf.append(String.format("%6s", "")); //返回码
+        buf.append(String.format("%100s", ""));
+        
+        buf.append(0); //后续包标志
+        buf.append(String.format("%03d", 0));//请求次数
+        buf.append("0");//签名标识 0不签
+        buf.append("1");//签名数据包格式
+        buf.append(String.format("%12s", "")); //签名算法
+        buf.append(String.format("%010d", 0)); //签名数据长度
+        buf.append(0);//附件数目
+        buf.append(xmlBody);//报文体
+        
+        return buf.toString();
+    }
+    
 	  
 }
